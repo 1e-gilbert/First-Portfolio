3ffdac38a7ee5f7fedc632364c552310d7e84df5
1e-gilbert <135290424+1e-gilbert@users.noreply.github.com>
2024-09-05 11:38:41 +0000

    Portfolio
    My first ever website using HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
